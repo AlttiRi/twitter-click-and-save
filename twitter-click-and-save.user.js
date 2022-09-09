0675589eff5fd76797f7e8ef67598c92713fc278
[Alt'tiRi] <16310547+AlttiRi@users.noreply.github.com>
2022-09-09 18:19:59 +0300

    Bump version

--- twitter-click-and-save.user.js
+++ twitter-click-and-save.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Twitter Click'n'Save
-// @version     0.8.3-2022.08.08-dev
+// @version     0.8.3-2022.08.09
 // @namespace   gh.alttiri
 // @description Add buttons to download images and videos in Twitter, also does some other enhancements.
 // @match       https://twitter.com/*
